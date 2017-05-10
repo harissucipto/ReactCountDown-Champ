72882d9f0e131531e9cf118029caab4320471882
Haris Sucipto <harisssucipto@gmail.com>
2017-05-11 03:50:57 +0700

    add changeDeadline method"

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deadline: 'December 25, 2017'
+      deadline: 'December 25, 2017',
+      newDeadline: ''
     }
+  }
+
+  changeDeadline() {
+    console.log(this.state);
+    this.setState({deadline: this.state.newDeadline});
   }
 
   render() {
@@ -20,8 +26,11 @@ class App extends Component {
           <div className="Clock-seconds">20 Seconds</div>
         </div>
         <div>
-          <input placeholder="new date" />
-          <button>Submit</button>
+          <input
+            placeholder="new date"
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
+          <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
     )
